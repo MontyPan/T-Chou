150bf82f293f5a4a16947fe9e6b387a354d95618
cwchou2016 <s955401@gmail.com>
2018-11-01 00:00:18 +0800

    Add function of placeStone

--- TicTacToe/src/us/dontcareabout/chou/ticTacToe/Main.java
+++ TicTacToe/src/us/dontcareabout/chou/ticTacToe/Main.java
@@ -16,16 +16,32 @@ public class Main {
 		char [][] playBoard = {{'-', '-','-'},{'-', '-','-'},{'-', '-','-'}};
 		
 		printBoard(playBoard);
-
+		
+		playBoard = placeStone(0,0, 2, playBoard);
+		printBoard(playBoard);
 
 	}
 	
+	// print the Tic Tac Toe game board
 	private static void printBoard(char [][] playBoard) {
 		int len = playBoard.length;
 		System.out.println("Tic Tac Toe");
 		for(int line=0; line<len; line++) {
 			System.out.println(playBoard[line]);
 		}
+		System.out.println();
+	}
+	
+
+	// place stone at position x, y. Player 1's mark ='O', Player 2's mark='X'
+	private static char [][] placeStone(int x, int y, int player, char [][] playBoard) {
+		char mark='-';
+		switch(player) {
+		case(1): {mark='O';break;}
+		case(2): {mark='X';break;}
+		}
+		playBoard[x][y] = mark;
+		return playBoard;
 	}
 
 	private static int getInputNumber() {
