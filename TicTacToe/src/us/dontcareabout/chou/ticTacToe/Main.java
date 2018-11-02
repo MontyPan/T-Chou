70377ba015661d04e1d148172b09c2dd67ad4052
cwchou2016 <s955401@gmail.com>
2018-11-02 20:41:56 +0800

    Unify coding style

--- TicTacToe/src/us/dontcareabout/chou/ticTacToe/Main.java
+++ TicTacToe/src/us/dontcareabout/chou/ticTacToe/Main.java
@@ -4,77 +4,117 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		char [][] playBoard = {{'-','-','-'},{'-','-','-'},{'-','-','-'}};
+		char[][] playBoard = {{'-', '-', '-'}, {'-', '-', '-'}, {'-', '-', '-'}};
 		int player = 1;
 
 		printBoard(playBoard);
 
-        while(true) {
-            System.out.println("Player "+ player +" input:");
-            int [] pos = getPosition();
-            if(placeStone(pos[0],pos[1], player, playBoard)) {
-                player = switchPlayer(player);
-                System.out.println();
-                printBoard(playBoard);
-            }
-        }
+		while (true) {
+			System.out.println("Player " + player + " input:");
+			int[] pos = getPosition();
+
+			if (placeStone(pos[0], pos[1], player, playBoard)) {
+				player = switchPlayer(player);
+				System.out.println();
+				printBoard(playBoard);
+			}
+		}
 	}
 
 	// Get position i, j from numpad
-	private static int [] getPosition() {
-	    int num = getInputNumber();
-	    int [] pos = {10,10};
-	    switch(num) {
-            case(1): {pos[0]=2; pos[1]=0; break;}
-            case(2): {pos[0]=2; pos[1]=1; break;}
-            case(3): {pos[0]=2; pos[1]=2; break;}
-            case(4): {pos[0]=1; pos[1]=0; break;}
-            case(5): {pos[0]=1; pos[1]=1; break;}
-            case(6): {pos[0]=1; pos[1]=2; break;}
-            case(7): {pos[0]=0; pos[1]=0; break;}
-            case(8): {pos[0]=0; pos[1]=1; break;}
-            case(9): {pos[0]=0; pos[1]=2; break;}
-        }
-        return pos;
-    }
+	private static int[] getPosition() {
+		int num = getInputNumber();
+		int[] pos = {10, 10};
+
+		switch (num) {
+			case (1): {
+				pos[0] = 2;
+				pos[1] = 0;
+				break;
+			}
+			case (2): {
+				pos[0] = 2;
+				pos[1] = 1;
+				break;
+			}
+			case (3): {
+				pos[0] = 2;
+				pos[1] = 2;
+				break;
+			}
+			case (4): {
+				pos[0] = 1;
+				pos[1] = 0;
+				break;
+			}
+			case (5): {
+				pos[0] = 1;
+				pos[1] = 1;
+				break;
+			}
+			case (6): {
+				pos[0] = 1;
+				pos[1] = 2;
+				break;
+			}
+			case (7): {
+				pos[0] = 0;
+				pos[1] = 0;
+				break;
+			}
+			case (8): {
+				pos[0] = 0;
+				pos[1] = 1;
+				break;
+			}
+			case (9): {
+				pos[0] = 0;
+				pos[1] = 2;
+				break;
+			}
+		}
+		return pos;
+	}
 
 	// print TicTacToe board
-	private static void printBoard( char [][] playBoard) {
-	    int len = playBoard.length;
-	    System.out.println("TicTacToe:");
-	    for(int idx=0; idx<len; idx++) {
-	        System.out.println(playBoard[idx]);
-        }
-    }
+	private static void printBoard(char[][] playBoard) {
+		int len = playBoard.length;
+		System.out.println("TicTacToe:");
 
-    // Switch players between 1 and 2
-    private static int switchPlayer(int player) {
-	    if(player == 1) {
-	        player =2;
-        } else if(player == 2) {
-	        player =1;
-        }
-        return player;
-    }
+		for (int idx = 0; idx < len; idx++) {
+			System.out.println(playBoard[idx]);
+		}
+	}
 
-    // place stones on position i, j.
-    // Return true if the position i, j is empty where stones can be placed.
-    private static boolean placeStone(int i, int j, int player, char [][] playBoard) {
-	    char marker = '-';
-	    if(player == 1) {
-	        marker = 'O';
-        } else {
-	        marker ='X';
-        }
+	// Switch players between 1 and 2
+	private static int switchPlayer(int player) {
+		if (player == 1) {
+			player = 2;
+		} else if (player == 2) {
+			player = 1;
+		}
+		return player;
+	}
 
-        if(playBoard[i][j]=='-') {
-            playBoard[i][j] = marker;
-            return true;
-        } else {
-            System.out.println("The position "+ i+", "+j +" is not available.");
-            return false;
-        }
-    }
+	// place stones on position i, j.
+	// Return true if the position i, j is empty where stones can be placed.
+	private static boolean placeStone(int i, int j, int player, char[][] playBoard) {
+		char marker = '-';
+
+		if (player == 1) {
+			marker = 'O';
+		} else {
+			marker = 'X';
+		}
+
+		if (playBoard[i][j] == '-') {
+			playBoard[i][j] = marker;
+			return true;
+		} else {
+			System.out.println("The position " + i + ", " + j + " is not available.");
+			return false;
+		}
+	}
 
 	@SuppressWarnings("resource")
 	private static int getInputNumber() {
@@ -82,11 +122,4 @@ public class Main {
 		return scanner.nextInt();
 	}
 
-	private static void callMethod(int value) {
-		for (int i = 0; i < value / 2; i++) {
-			System.out.print("*");
-		}
-
-		System.out.println();
-	}
 }
