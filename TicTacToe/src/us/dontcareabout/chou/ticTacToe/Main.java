5736018656c43a0fef5a9ec000ef4485f06d7d31
cwchou2016 <s955401@gmail.com>
2018-11-02 09:55:58 +0800

    Add function of switchPlayer

--- TicTacToe/src/us/dontcareabout/chou/ticTacToe/Main.java
+++ TicTacToe/src/us/dontcareabout/chou/ticTacToe/Main.java
@@ -11,25 +11,36 @@ public class Main {
 
         placeStone(0,0, player, playBoard);
         printBoard(playBoard);
+        player=switchPlayer(player);
 
         placeStone(0,1, player, playBoard);
         printBoard(playBoard);
 
-        placeStone(0,0, player, playBoard);
+        placeStone(1,0, player, playBoard);
         printBoard(playBoard);
 	}
 
 	// print TicTacToe board
-	public static void printBoard( char [][] playBoard) {
+	private static void printBoard( char [][] playBoard) {
 	    int len = playBoard.length;
 	    for(int idx=0; idx<len; idx++) {
 	        System.out.println(playBoard[idx]);
         }
     }
 
+    // Switch players between 1 and 2
+    private static int switchPlayer(int player) {
+	    if(player == 1) {
+	        player =2;
+        } else if(player == 2) {
+	        player =1;
+        }
+        return player;
+    }
+
     // place stones on position i, j.
     // Return true if the position i, j is empty where stones can be placed.
-    public static boolean placeStone(int i, int j, int player, char [][] playBoard) {
+    private static boolean placeStone(int i, int j, int player, char [][] playBoard) {
 	    char marker = '-';
 	    if(player == 1) {
 	        marker = 'O';
