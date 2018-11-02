47ba486664cd31163fa3a6ad8a8aef813b7adf52
cwchou2016 <s955401@gmail.com>
2018-11-02 09:51:41 +0800

    Add function of placeStones

--- TicTacToe/src/us/dontcareabout/chou/ticTacToe/Main.java
+++ TicTacToe/src/us/dontcareabout/chou/ticTacToe/Main.java
@@ -5,8 +5,18 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		char [][] playBoard = {{'-','-','-'},{'-','-','-'},{'-','-','-'}};
+		int player = 1;
 
 		printBoard(playBoard);
+
+        placeStone(0,0, player, playBoard);
+        printBoard(playBoard);
+
+        placeStone(0,1, player, playBoard);
+        printBoard(playBoard);
+
+        placeStone(0,0, player, playBoard);
+        printBoard(playBoard);
 	}
 
 	// print TicTacToe board
@@ -14,6 +24,25 @@ public class Main {
 	    int len = playBoard.length;
 	    for(int idx=0; idx<len; idx++) {
 	        System.out.println(playBoard[idx]);
+        }
+    }
+
+    // place stones on position i, j.
+    // Return true if the position i, j is empty where stones can be placed.
+    public static boolean placeStone(int i, int j, int player, char [][] playBoard) {
+	    char marker = '-';
+	    if(player == 1) {
+	        marker = 'O';
+        } else {
+	        marker ='X';
+        }
+
+        if(playBoard[i][j]=='-') {
+            playBoard[i][j] = marker;
+            return true;
+        } else {
+            System.out.println("The position i, j is not available.");
+            return false;
         }
     }
 
