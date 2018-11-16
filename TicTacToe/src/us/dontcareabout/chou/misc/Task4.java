42c26c7413b6c0751a87f8ccd73a307e29a04f99
MontyPan <monty.pan@dontcareabout.us>
2018-11-16 11:59:24 +0800

    Task-4 的空殼

---
+++ TicTacToe/src/us/dontcareabout/chou/misc/Task4.java
@@ -0,0 +1,20 @@
+package us.dontcareabout.chou.misc;
+
+import java.util.Arrays;
+
+public class Task4 {
+	public static void main(String[] args) {
+		int[] data = {5, 5, 6, 6, 78, 78, 78, 9487, 9487, 9487, 9487, 65535};
+		int[] result = longest(data);
+		System.out.println(Arrays.toString(result));
+	}
+
+	/**
+	 * @param data 已經排序過的 array
+	 * @return result[0] 是最長平台的數字，result[1] 是長度
+	 */
+	private static int[] longest(int[] data) {
+		//FIXME
+		return new int[]{9487, 4};
+	}
+}
