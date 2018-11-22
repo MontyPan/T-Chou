e29410fa580e7b0b3b3c47445efad6c94f42c6e6
cwchou2016 <s955401@gmail.com>
2018-11-22 22:10:09 +0800

    Replace FIXME in method longest

--- TicTacToe/src/us/dontcareabout/chou/misc/Task4.java
+++ TicTacToe/src/us/dontcareabout/chou/misc/Task4.java
@@ -14,7 +14,20 @@ public class Task4 {
 	 * @return result[0] 是最長平台的數字，result[1] 是長度
 	 */
 	private static int[] longest(int[] data) {
-		//FIXME
-		return new int[]{9487, 4};
+		int len = data.length;
+		int[] dummyTarget = new int[]{data[0], 1};
+		int[] target = new int[]{-1, 0};
+
+		for (int idx = 1; idx < len; idx++) {
+			if (data[idx] == dummyTarget[0]) {
+				dummyTarget[1] += 1;
+			} else {
+				if (dummyTarget[1] > target[1]) {
+					target = dummyTarget;
+				}
+				dummyTarget = new int[]{data[idx], 1};
+			}
+		}
+		return target;
 	}
 }
