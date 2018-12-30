8dfc189e8252ae0291e0b8ce360b55a0f44c2368
cwchou2016 <s955401@gmail.com>
2018-12-31 03:43:00 +0800

    Task 6 complete

--- TicTacToe/src/us/dontcareabout/chou/misc/Task6.java
+++ TicTacToe/src/us/dontcareabout/chou/misc/Task6.java
@@ -11,7 +11,16 @@ package us.dontcareabout.chou.misc;
  */
 public class Task6 {
 	public static void main(String[] args) {
-		System.out.println("task6");
+		System.out.println("Armstrong numbers:");
 
+		for (int i = 100; i < 1000; i++) {
+			int a = i / 100;
+			int b = (i % 100) / 10;
+			int c = i % 10;
+
+			if (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3) == i) {
+				System.out.println("" + a + b + c);
+			}
+		}
 	}
 }
