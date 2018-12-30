7cacfea8941db1bd8d3510d4efec61babeeb9fe3
cwchou2016 <s955401@gmail.com>
2018-12-31 04:16:01 +0800

    Add method initBoard

--- TicTacToe/src/us/dontcareabout/chou/ticTacToe/Board.java
+++ TicTacToe/src/us/dontcareabout/chou/ticTacToe/Board.java
@@ -1,7 +1,19 @@
 package us.dontcareabout.chou.ticTacToe;
 
 public class Board {
+	public static final int N = 3;
+	char[][] playBoard = new char[N][N];
+
 	public Board() {
 		System.out.println("Board");
+		initBoard();
+	}
+
+	public void initBoard() {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				playBoard[i][j] = ' ';
+			}
+		}
 	}
 }
