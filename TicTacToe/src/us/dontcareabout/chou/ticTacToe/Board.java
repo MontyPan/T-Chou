6a9f4f4607770ffd838bf8752d340e1a855fff01
cwchou2016 <s955401@gmail.com>
2018-12-31 04:39:11 +0800

    Add method printBoard

--- TicTacToe/src/us/dontcareabout/chou/ticTacToe/Board.java
+++ TicTacToe/src/us/dontcareabout/chou/ticTacToe/Board.java
@@ -16,4 +16,29 @@ public class Board {
 			}
 		}
 	}
+
+	/**
+	 * Print TicTacToe board
+	 */
+	public void printBoard() {
+
+		// create inputBoard
+		char[][] inputBoard = new char[N][N];
+
+		for (int idx1 = 0; idx1 < N; idx1++) {
+			for (int idx2 = 0; idx2 < N; idx2++) {
+				int number = idx1 * N + idx2 + 1;
+				inputBoard[idx1][idx2] = (char) (number + '0');
+			}
+		}
+
+		// print out
+		for (int idx = 0; idx < N; idx++) {
+			String board1 = String.valueOf(playBoard[idx]);
+			String board2 = String.valueOf(inputBoard[idx]);
+			String spaces = "       ";
+
+			System.out.println(board1 + spaces + board2);
+		}
+	}
 }
