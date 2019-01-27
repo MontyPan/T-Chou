371beb6719fe0acc8662083e089e36e4e7938d90
cwchou2016 <s955401@gmail.com>
2019-01-28 03:03:19 +0800

    add constructor in class ShowBoard

--- TicTacToe/src/us/dontcareabout/chou/ticTacToe/ShowBoard.java
+++ TicTacToe/src/us/dontcareabout/chou/ticTacToe/ShowBoard.java
@@ -1,40 +1,40 @@
 package us.dontcareabout.chou.ticTacToe;
 
 public class ShowBoard {
-	public void printBoard(Board board) {
-		int len = Board.N;
+	private int N = Board.N;
+	private char[][] inputBoard = new char[N][N];
+	private char[] rowSep = new char[N];
 
-		// create inputBoard
-		char[][] inputBoard = new char[len][len];
-		char[][] playBoard = new char[len][len];
-
-		for (int idx = 0; idx < len; idx++) {
-			for (int idx2 = 0; idx2 < len; idx2++) {
-				int fillNumber = idx * len + idx2 + 1;
+	public ShowBoard() {
+		for (int idx = 0; idx < N; idx++) {
+			for (int idx2 = 0; idx2 < N; idx2++) {
+				int fillNumber = idx * N + idx2 + 1;
 				inputBoard[idx][idx2] = (char) (fillNumber + '0');
+			}
+			rowSep[idx] = '-';
+		}
+	}
 
+	public void printBoard(Board board) {
+		char[][] playBoard = new char[N][N];
+
+		for (int idx = 0; idx < N; idx++) {
+			for (int idx2 = 0; idx2 < N; idx2++) {
 				Boolean value = board.getStatus(idx, idx2);
 				playBoard[idx][idx2] = getMark(value);
 			}
 		}
 
-		// seperator between row
-		char[] rowSep = new char[len];
-
-		for (int idx = 0; idx < len; idx++) {
-			rowSep[idx] = '-';
-		}
-
 		System.out.println("TicTacToe:");
 
 		// print out boards
-		for (int idx = 0; idx < len; idx++) {
+		for (int idx = 0; idx < N; idx++) {
 			String board1 = array2String(playBoard[idx], "|");
 			String board2 = array2String(inputBoard[idx], "|");
 			String spaces = "        ";
 			System.out.println(board1 + spaces + board2);
 
-			if (idx != len - 1) {
+			if (idx != N - 1) {
 				String grid = array2String(rowSep, "+");
 				System.out.println(grid + spaces + grid);
 			}
