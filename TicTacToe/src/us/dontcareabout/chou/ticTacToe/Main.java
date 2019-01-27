342fe33393717cc58d1c7458360a06a686f0669d
cwchou2016 <s955401@gmail.com>
2019-01-28 01:57:42 +0800

    move methods printBoard and array2String to class ShowBoard

--- TicTacToe/src/us/dontcareabout/chou/ticTacToe/Main.java
+++ TicTacToe/src/us/dontcareabout/chou/ticTacToe/Main.java
@@ -6,7 +6,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		Board board = new Board();
-		printBoard(board);
+		ShowBoard showBoard = new ShowBoard();
+		showBoard.printBoard(board);
 
 		int maxTurn = Board.N * Board.N;
 		int turns = 0;
@@ -24,7 +25,7 @@ public class Main {
 
 			if (board.placeStone(pos[0], pos[1])) {
 				System.out.println();
-				printBoard(board);
+				showBoard.printBoard(board);
 
 				if (board.checkWin(pos[0], pos[1])) {
 					System.out.println(playerName + " wins!");
@@ -65,65 +66,6 @@ public class Main {
 
 		return pos;
 	}
-
-	// print TicTacToe board
-	private static void printBoard(Board board) {
-		int len = Board.N;
-
-		// create inputBoard
-		char[][] inputBoard = new char[len][len];
-		char[][] playBoard = new char[len][len];
-
-		for (int idx = 0; idx < len; idx++) {
-			for (int idx2 = 0; idx2 < len; idx2++) {
-				int fillNumber = idx * len + idx2 + 1;
-				inputBoard[idx][idx2] = (char) (fillNumber + '0');
-
-				Boolean value = board.getStatus(idx, idx2);
-				playBoard[idx][idx2] = String.valueOf(value).charAt(0);
-			}
-		}
-
-		// seperator between row
-		char[] rowSep = new char[len];
-
-		for (int idx = 0; idx < len; idx++) {
-			rowSep[idx] = '-';
-		}
-
-		System.out.println("TicTacToe:");
-
-		// print out boards
-		for (int idx = 0; idx < len; idx++) {
-			String board1 = array2String(playBoard[idx], "|");
-			String board2 = array2String(inputBoard[idx], "|");
-			String spaces = "        ";
-			System.out.println(board1 + spaces + board2);
-
-			if (idx != len - 1) {
-				String grid = array2String(rowSep, "+");
-				System.out.println(grid + spaces + grid);
-			}
-		}
-	}
-
-	/**
-	 * Convert a char array to a string
-	 */
-	private static String array2String(char[] array, String sep) {
-		int len = array.length;
-		String msg = "";
-
-		for (int idx = 0; idx < len; idx++) {
-			msg += array[idx];
-
-			if (idx != len - 1) {
-				msg += sep;
-			}
-		}
-		return msg;
-	}
-
 
 	@SuppressWarnings("resource")
 	private static int getInputNumber() throws Exception {
--- TicTacToe/src/us/dontcareabout/chou/ticTacToe/ShowBoard.java
+++ TicTacToe/src/us/dontcareabout/chou/ticTacToe/ShowBoard.java
@@ -1,5 +1,60 @@
 package us.dontcareabout.chou.ticTacToe;
 
 public class ShowBoard {
+	public void printBoard(Board board) {
+		int len = Board.N;
 
+		// create inputBoard
+		char[][] inputBoard = new char[len][len];
+		char[][] playBoard = new char[len][len];
+
+		for (int idx = 0; idx < len; idx++) {
+			for (int idx2 = 0; idx2 < len; idx2++) {
+				int fillNumber = idx * len + idx2 + 1;
+				inputBoard[idx][idx2] = (char) (fillNumber + '0');
+
+				Boolean value = board.getStatus(idx, idx2);
+				playBoard[idx][idx2] = String.valueOf(value).charAt(0);
+			}
+		}
+
+		// seperator between row
+		char[] rowSep = new char[len];
+
+		for (int idx = 0; idx < len; idx++) {
+			rowSep[idx] = '-';
+		}
+
+		System.out.println("TicTacToe:");
+
+		// print out boards
+		for (int idx = 0; idx < len; idx++) {
+			String board1 = array2String(playBoard[idx], "|");
+			String board2 = array2String(inputBoard[idx], "|");
+			String spaces = "        ";
+			System.out.println(board1 + spaces + board2);
+
+			if (idx != len - 1) {
+				String grid = array2String(rowSep, "+");
+				System.out.println(grid + spaces + grid);
+			}
+		}
+	}
+
+	/**
+	 * Convert a char array to a string
+	 */
+	private String array2String(char[] array, String sep) {
+		int len = array.length;
+		String msg = "";
+
+		for (int idx = 0; idx < len; idx++) {
+			msg += array[idx];
+
+			if (idx != len - 1) {
+				msg += sep;
+			}
+		}
+		return msg;
+	}
 }
