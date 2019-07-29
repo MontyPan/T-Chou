eaf1b6599967688a69829bd044ccf2355dff9440
cwchou2016 <s955401@gmail.com>
2019-07-30 03:38:39 +0800

    新增 addChangeInfo() 加入換展資訊

--- GoogleSheet/src/main/java/us/dontcareabout/googleSheet/Exhibition.java
+++ GoogleSheet/src/main/java/us/dontcareabout/googleSheet/Exhibition.java
@@ -19,9 +19,32 @@ public class Exhibition {
 		this.rooms = Arrays.asList(data.rooms.split(","));
 	}
 
+	/**
+	 * 新增換展資訊
+	 */
+	public void addChangeInfo(RawData data) {
+		closeIntervals.add(new CloseInterval(data));
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Exhibition:\nName: %s\nDate: %s ~ %s\nLocation: %s\nChangeInfo:\n%s\n", name, start, end, rooms, closeIntervals);
+	}
+
 	private class CloseInterval {
 		Date start;
 		Date end;
 		String rooms;
+
+		CloseInterval(RawData data) {
+			this.start = data.start;
+			this.end = data.end;
+			this.rooms = data.rooms;
+		}
+
+		@Override
+		public String toString() {
+			return String.format("(%s ~ %s @%s)\n", start, end, rooms);
+		}
 	}
 }
