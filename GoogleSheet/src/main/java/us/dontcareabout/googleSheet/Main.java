65b1ac3485923a5b04fd37a4df2e6bd4a1a64b44
cwchou2016 <s955401@gmail.com>
2019-08-04 19:26:44 +0800

    新增 getRoomInfo()

--- GoogleSheet/src/main/java/us/dontcareabout/googleSheet/Main.java
+++ GoogleSheet/src/main/java/us/dontcareabout/googleSheet/Main.java
@@ -2,15 +2,21 @@ package us.dontcareabout.googleSheet;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 	private final static String SHEET_ID = "1xRkXYlAioJe44AvhLpxuyZLfkvkmz9L0sPZo67feKH0";
+	private final static String[] ALL_ROOMS = {"S101", "S102", "S201", "S202", "S203", "S204", "S301", "S302", "S303", "S304"};
 
 	public static void main(String[] args) {
 		ArrayList<RawData> dataTable = new Mapper<RawData>(SHEET_ID, 1).result;
 
 		for (Exhibition e : getExhibitionInfo(dataTable)) {
 			System.out.println(e);
+		}
+
+		for (ShowRoom sr : getRoomInfo(dataTable).values()) {
+			System.out.println(sr);
 		}
 	}
 
@@ -37,5 +43,20 @@ public class Main {
 			if (!gotInfo) throw new RuntimeException(String.format("%s not exists", d.name));
 		}
 		return exhibitions;
+	}
+
+	public static Map<String, ShowRoom> getRoomInfo(List<RawData> dataTable) {
+		Map<String, ShowRoom> showRoomMap = ShowRoom.createShowRooms(ALL_ROOMS);
+
+		for (RawData d : dataTable) {
+			for (String r : ShowRoom.roomAsList(d.rooms)) {
+				if (d.close) {
+					showRoomMap.get(r).addClose(d.name, d.start, d.end);
+					break;
+				}
+				showRoomMap.get(r).addOpen(d.name, d.start, d.end);
+			}
+		}
+		return showRoomMap;
 	}
 }
