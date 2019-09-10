277edcbe3830b322e2fdec0af36181f916ba8638
cwchou2016 <s955401@gmail.com>
2019-09-11 04:31:44 +0800

    新增 setter

--- GoogleSheet/src/main/java/us/dontcareabout/googleSheet2/DateInterval.java
+++ GoogleSheet/src/main/java/us/dontcareabout/googleSheet2/DateInterval.java
@@ -9,12 +9,9 @@ public class DateInterval {
 	private Date start;
 	private Date end;
 
-	public DateInterval(Date start, Date end) {
-		if (start.after(end)) {
-			throw new DateIntervalException("Start date is not earlier than end date");
-		}
-		this.start = start;
-		this.end = end;
+	public DateInterval(Date date1, Date date2) {
+		this.start = date1.before(date2) ? date1 : date2;
+		this.end = date1.after(date2) ? date1 : date2;
 	}
 
 	public Date getStart() {
@@ -25,11 +22,19 @@ public class DateInterval {
 		return end;
 	}
 
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+	public void setEnd(Date end) {
+		this.end = end;
+	}
+
 	public boolean containInterval(DateInterval innerInterval) {
-		if (start.getTime() > innerInterval.start.getTime()) {
+		if (start.after(innerInterval.getStart())) {
 			return false;
 		}
-		if (end.getTime() < innerInterval.end.getTime()) {
+		if (end.before(innerInterval.getEnd())) {
 			return false;
 		}
 		return true;
